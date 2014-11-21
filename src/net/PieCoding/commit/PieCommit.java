b237cca4e00e853d2831be5a4e477e2030a7ca86
Brandon <kellatech38@gmail.com>
2014-11-21 18:44:28 +0000

    Create PieCommit.java

---
+++ src/net/PieCoding/commit/PieCommit.java
@@ -0,0 +1,17 @@
+package net.PieCoding.commit
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PieCommit extends JavaPlugin
+
+public void OnEnable()
+{
+
+}
+
+public void OnDisable()
+{
+
+}
+
+//No idea what to do here yet will do later :D
