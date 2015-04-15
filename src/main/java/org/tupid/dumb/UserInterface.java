02b3d1c3255116ecda44c6f8aebc681c2deb4a52
Paul Reilly <paldiutheminecrafter@gmail.com>
2015-04-15 12:24:08 -0500

    Create UserInterface.java
    heuheauheuh

---
+++ src/main/java/org/tupid/dumb/UserInterface.java
@@ -0,0 +1,14 @@
+package org.tupid.dumb;
+
+public interface UserInterface<T> {
+    public static void getUser(Object user);
+    public static void getOS(String opSys);
+    public static void getClass(Class<T> class);
+    public static void shutDown(Runtime run, System method);
+    int runtime(double time);
+    int runtime(long time);
+    int runtime(float time);
+    public static void useless(String useless, Object[] fill, ArrayList<> pointless);
+    
+    public Enum huehuehue { YES, NO, DIE, FOOT }
+}
