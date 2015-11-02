0db8c0a4d57f5b40d1725c5051520a170c622d21
Ronit Sajeey (ItzLevvie) <rsajeey@hotmail.com>
2015-11-02 12:45:42 +0000

    Update PieCommit.java

--- src/net/PieCoding/commit/PieCommit.java
+++ src/net/PieCoding/commit/PieCommit.java
@@ -1,17 +1,32 @@
 package net.PieCoding.commit
-
-import org.bukkit.plugin.java.JavaPlugin;
+// Welcome to PieCommit where half of the code is fake!!!±! ahahhahahah
+import loadPackages() // lLOLLOLOLOLOLOLOLOLOLOLoLOLOLOLOLOLOLO
 
 public class PieCommit extends JavaPlugin
 {
   @Override
   public void onEnable()
   {
-  
+    Bukkit.broadcastMessage("wilieeeeeeeeee has been enabled!");
+    loadWiliee(); // load wiliee
+    if(williee.isDisabled) {
+      Bukkit.getPluginManager().load("PieCommit").authors("WALCOME PIEEEEEESSSSSS NOTTTTT ALLLLOWWWWWEEEEEDDDD").version("v6.8").package("org.piecommit.piecommit.load.modules")''
+    }
+    server.shutdown(); // HAHAHAH i'M eVIllaALal
   }
   @Override
   public void onDisable()
   {
+    loadWilliee();
+    Bukkit.broadcastMessage("wilieeeeeeee has been disabled! everything is successfully unloaded!!!!!");
+    server.shutdown();
+  }
   
+  public static void loadWiliee() {
+    Bukkit.broadcastMessage("wiliee loaded loaded plugieeenenennene");
+  }
+  
+  public static void unloadWiliee() {
+    Bukkit.broadcastMessage("wiliee loaded loaded plugieeenenennene");
   }
 }
