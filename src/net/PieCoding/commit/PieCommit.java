9d7328da8b29c75795f8557c4bd5af440d11a93f
Ronit Sajeey (ItzLevvie) <rsajeey@hotmail.com>
2015-11-12 12:42:47 +0000

    Update PieCommit.java

--- src/net/PieCoding/commit/PieCommit.java
+++ src/net/PieCoding/commit/PieCommit.java
@@ -1,31 +1,24 @@
 package net.PieCoding.commit
 import com.Castia994.PieCommit.modules
 modules.loadPackage("org.PieCommit.PieCommit.load.imports")
-public class PieCommit extends JavaPlugin
-{
-  @Override
-	public void onEnable()
-	{
+public class PieCommit extends JavaPlugin {@Override
+	public void onEnable() {
 		loadWilliee();
 		Bukkit.broadcastMessage("wilieeeeeeeeee has been enabled!");
-		if (Williee.isDisabled())
-		{
+		if (Williee.isDisabled()) {
 			Bukkit.getPluginManager().load("PieCommit").authors("PieGuy7896 aka. TheGeneralBits1349").version("v6.8").package("org.PieCommit.PieCommit.load.modules");
 		}
 		server.shutdown();
 	}@Override
-	public void onDisable()
-	{
+	public void onDisable() {
 		unloadWilliee();
 		Bukkit.broadcastMessage("wilieeeeeeee has been disabled! everything is successfully unloaded!!!!!");
-		if (Williee.isEnabled())
-		{
+		if (Williee.isEnabled()) {
 			Bukkit.getPluginManager().unload("PieCommit").authors("PieGuy7896 aka. TheGeneralBits1349").version("v6.8").package("org.PieCommit.PieCommit.unload.modules");
 		}
 		server.shutdown();
 	}
-	public static void loadWilliee()
-	{
+	public static void loadWilliee() {
 		Bukkit.broadcastMessage("wiliee loaded loaded plugieeenenennene");
 		Bukkit.broadcastMessage("wiliee loaded loaded plugieeenenennene");
 		Bukkit.broadcastMessage("wiliee loaded loaded plugieeenenennene");
@@ -69,8 +62,7 @@ public class PieCommit extends JavaPlugin
 		Bukkit.broadcastMessage("wiliee loaded loaded plugieeenenennene");
 		Bukkit.broadcastMessage("wiliee loaded loaded plugieeenenennene");
 	}
-	public static void unloadWilliee()
-	{
+	public static void unloadWilliee() {
 		Bukkit.broadcastMessage("wiliee unloaded unloaded plugieeenenennene");
 		Bukkit.broadcastMessage("wiliee unloaded unloaded plugieeenenennene");
 		Bukkit.broadcastMessage("wiliee unloaded unloaded plugieeenenennene");
