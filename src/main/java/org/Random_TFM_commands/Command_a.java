c34a323d4835936ecd9a2db69ceef2dcf159cbae
Ronit Sajeey (ItzLevvie) <rsajeey@hotmail.com>
2015-11-23 13:05:05 +0000

    Update and rename Command_hack.java to Command_a.java

--- src/main/java/org/Random_TFM_commands/Command_a.java
+++ src/main/java/org/Random_TFM_commands/Command_a.java
@@ -1,21 +1,16 @@
 package org.Random_TFM_Commands;
 
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.ALL, source = SourceType.ONLY_IN_GAME)
 @CommandParameters(description = "Because I have nothing better to do.", usage = "/<command>")
-public class Command_hack extends TFM_Command
+public class Command_a extends CommandExecutor<Exts4>
 {
-    
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        sender_p.kickPlayer("m8 hacking is for nubs your Idiot");
+        sender.sendMessage("");
         return true;
-       
     }
 }
-hi i'm breaking it
