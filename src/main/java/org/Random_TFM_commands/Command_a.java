871691b742af6597dbcf4edfef295acbaccaa5d3
Ronit Sajeey (ItzLevvie) <rsajeey@hotmail.com>
2015-11-25 12:32:16 +0000

    Update Command_a.java

--- src/main/java/org/Random_TFM_commands/Command_a.java
+++ src/main/java/org/Random_TFM_commands/Command_a.java
@@ -4,11 +4,11 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
 @CommandPermissions(level = AdminLevel.ALL, source = SourceType.ONLY_IN_GAME)
-@CommandParameters(description = "Because I have nothing better to do.", usage = "/<command>")
-public class Command_a extends CommandExecutor<Exts4>
+@CommandParameters(description = "", usage = "/<command>")
+public class Command_a extends CommandExecutor<E>
 {
     @Override
-    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    public boolean onCommand(CommandSender sender, Player sender, Command cmd, String commandLabel, String[] args)
     {
         sender.sendMessage("");
         return true;
