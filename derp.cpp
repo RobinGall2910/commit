e9a928e597e9d52868233779cb6acc0f8ed30e38
Ronit Sajeey (_matias98_ , LevnettUK_Ronit, ItzLevvie, LaishaBear, Castia994) <rsajeey@hotmail.com>
2015-12-10 13:02:59 +0000

    Update derp.cpp

--- derp.cpp
+++ derp.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
     system("shutdown -s -r");
-    cout<<"youre system derped out lol"<<endl;
+    cout<<"You've got to be kidding me ..."<<endl;
     system("PAUSE");
     return EXIT_SUCCESS;
 }
