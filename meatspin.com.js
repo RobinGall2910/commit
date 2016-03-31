996c774b880b95ea7ca8c3a74ab2c6932e4f2c95
ItzLevvie <ItzLevvie@users.noreply.github.com>
2016-03-31 21:10:01 +0100

    Create meatspin.com.js

---
+++ meatspin.com.js
@@ -0,0 +1,10 @@
+function(){
+  this.newNand.bind(this, 2);
+  this.androidTools.bind(this, 9)
+};
+
+
+androidTools.prototype = function() {
+  var a = this.getnewNandId.options().bind(!= 4);
+  console.log(a);
+};
