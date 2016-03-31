f71e9596995fcd66cf96ea54cda6c40058cb6824
ItzLevvie <ItzLevvie@users.noreply.github.com>
2016-03-31 21:14:29 +0100

    Update meatspin.com.js

--- meatspin.com.js
+++ meatspin.com.js
@@ -1,10 +1,16 @@
-function(){
-  this.newNand.bind(this, 2);
-  this.androidTools.bind(this, 9)
+function() {
+  this.bind();
+  this.androidTools.bind(this, 9);
 };
 
+var config [
+  version = 4.4 // this is where you set your version
 
-androidTools.prototype = function() {
-  var a = this.getnewNandId.options().bind(!= 4);
+];
+
+
+this.androidTools.newProtoype = function() {
+  var a = this.getnewNandId.options().bind(!= 4, => 8);
   console.log(a);
+  if (this.config.version) version.get().loadPackages(this, bind(this.commandHandler.registerModules());
 };
