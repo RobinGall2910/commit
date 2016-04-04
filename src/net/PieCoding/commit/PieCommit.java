1d688e064fb0c23b7ce786e9fe1f6c221293a97e
ItzLevvie <ItzLevvie@users.noreply.github.com>
2016-04-04 21:01:57 +0100

    revert broken stuff

--- src/net/PieCoding/commit/PieCommit.java
+++ src/net/PieCoding/commit/PieCommit.java
@@ -1,5 +1,5 @@
 package net.PieCoding.commit
-// Welcome to PieCommit where half of the code is non-existent!!!±! ahahhahahah
+// Welcome to PieCommit where half of the code is fake!!!±! ahahhahahah
 /*
  *
  * Copyright 1996 - 2014 <usrName()>
@@ -8,19 +8,18 @@ package net.PieCoding.commit
  * 
  */
 
-import com.Castia994.PieCommit.modules;
-modules.loadPackage("org.PieCommit.PieCommit.load.imports"); // lLOLLOLOLOLOLOLOLOLOLOLoLOLOLOLOLOLOLO
+import com.Castia994.PieCommit.modules
+modules.loadPackage("org.PieCommit.PieCommit.load.imports") // lLOLLOLOLOLOLOLOLOLOLOLoLOLOLOLOLOLOLO
 
 public class PieCommit extends JavaPlugin
 {
   @Override
-  public void onEnable(checkisEnabled)
+  public void onEnable()
   {
-      public static checkisEnabled p = Bukkit.getPluginManager.checkPlugin("PieCommit); //lol i'm so mixed up with javascript and java xD
-      loadWilliee(); // make it so it loads first (loading wilieees :S:D::D)
-      Bukkit.broadcastMessage("wilieeeeeeeeee v1.0 has been enabled!");
-      if (p.isDisabled())
-Bukkit.getPluginManager().load("PieCommit").authors("PieGuy7896 aka. TheGeneralBits1349").version("v6.8").package("org.PieCommit.PieCommit.modules.loadSetup");
+    loadWilliee(); // make it so it loads first (loading wilieees :S:D::D)
+    Bukkit.broadcastMessage("wilieeeeeeeeee has been enabled!");
+    if(Williee.isDisabled()) {
+      Bukkit.getPluginManager().load("PieCommit").authors("PieGuy7896 aka. TheGeneralBits1349").version("v6.8").package("org.PieCommit.PieCommit.load.modules");
     } // Loads' the plugin if it's disabled from plugman
     server.shutdown(); // HAHAHAH i'M eVIllaALal
   }
@@ -29,12 +28,11 @@ Bukkit.getPluginManager().load("PieCommit").authors("PieGuy7896 aka. TheGeneralB
   public void onDisable()
   {
     unloadWilliee(); // unloaD WILIEES
-    Bukkit.broadcastMessage("wilieeeeeeee v1.0 has been disabled! everything is successfully unloaded!!!!!");
+    Bukkit.broadcastMessage("wilieeeeeeee has been disabled! everything is successfully unloaded!!!!!");
     if(Williee.isEnabled()) {
-      Bukkit.getPluginManager().unload("PieCommit").authors("PieGuy7896 aka. TheGeneralBits1349").version("v6.8").package("org.PieCommit.PieCommit.modules
-stopSetup");
-      } // Un-loads' the plugin if it's loaded from plugman
-      server.shutdown(); // Shuts' down the server.
+      Bukkit.getPluginManager().unload("PieCommit").authors("PieGuy7896 aka. TheGeneralBits1349").version("v6.8").package("org.PieCommit.PieCommit.unload.modules");
+    } // Un-loads' the plugin if it's loaded from plugman
+    server.shutdown(); // Shuts' down the server.
   }
   
   public static void loadWilliee() { // TODO: Use INT to handle this behaviour.
